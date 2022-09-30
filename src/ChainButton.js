8bf30afc29fa3564e82431a66582728cabb10719
gerardo170 <90517150+gerardo170@users.noreply.github.com>
2022-09-30 11:52:34 -0500

    Add files via upload

---
+++ src/ChainButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from 'reactstrap';
+
+function ChainButton() {
+    return (
+        <Button color="primary">Ingresar nuevo usuario</Button>
+    );
+}
+
+export { ChainButton };
---
+++ src/ChainSearch.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ChainSearch(){
+    return(
+        <input placeholder="Buscar" />
+    );
+}
+
+export {ChainSearch};
